c32734ea7b1b69a6c9c555ee2b2b020cace3754f
quanganh <quanganhdoan1299.vn@gmail.com>
2025-07-29 23:55:38 +0700

    video #45

---
+++ src/config/database.ts
@@ -0,0 +1,26 @@
+// Get the client
+import mysql from 'mysql2/promise';
+
+// Create a connection to database
+const getConnection = async () => {
+    const connection = await mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'nodejspro',
+    })
+
+    try {
+        const [results, fields] = await connection.query(
+            'SELECT * FROM `users`'
+        );
+
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export default getConnection;
