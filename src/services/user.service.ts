aef46517b6bf41e261bc4b6413f90968ba19ee32
quanganh <quanganhdoan1299.vn@gmail.com>
2025-08-07 21:02:02 +0700

    feat: create users

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,20 @@
 import getConnection from "../config/database"
 
-const handleCreateUser = (fullName: string, email: string, address: string) => {
-    // insert into database
+const handleCreateUser = async (fullName: string, email: string, address: string) => {
+    const connection = await getConnection();
 
-    // return result
-    console.log(">> insert a new user")
+    // execute will internally call prepare and query
+    try {
+        const sql = 'INSERT INTO `users`(`name`, `email`, `address`) VALUES (?, ?, ?)';
+        const values = [fullName, email, address];
+
+        const [result, fields] = await connection.execute(sql, values);
+        return result;
+    }
+    catch (err) {
+        console.log(err);
+        return [];
+    }
 }
 
 const getAllUsers = async () => {
@@ -19,7 +29,6 @@ const getAllUsers = async () => {
         console.log(err);
         return [];
     }
-    return "hoidanit"
 }
 
 export { handleCreateUser, getAllUsers }
