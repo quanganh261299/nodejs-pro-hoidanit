d096e2d51d25075c08f50936c5f1e31237a29b87
quanganh <quanganhdoan1299.vn@gmail.com>
2025-09-04 00:08:34 +0700

    init: init zod and zod type

--- src/controllers/admin/product.controller.ts
+++ src/controllers/admin/product.controller.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from "express"
+import { ProductSchema, TProductSchema } from "src/validation/product.schema";
 
 const getAdminCreateProductPage = async (req: Request, res: Response) => {
     return res.render('admin/product/create.ejs')
 }
 
 const postAdminCreateProduct = async (req: Request, res: Response) => {
-    const { name } = req.body
+    const { name, } = req.body as TProductSchema
+
+    try {
+        const result = ProductSchema.parse(req.body)
+        console.log('run ok: ', result)
+    } catch (error) {
+        console.log(error)
+    }
 
     return res.redirect('/admin/product')
 }
