d96c08d1c2b01eb30fe85275b8f697b870d101b2
quanganh <quanganhdoan1299.vn@gmail.com>
2025-10-17 23:50:12 +0700

    update: login and register UI

---
+++ src/controllers/client/auth.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express"
+
+
+const getRegisterPage = async (req: Request, res: Response) => {
+    return res.render("client/auth/register.ejs")
+}
+
+const getLoginPage = async (req: Request, res: Response) => {
+    return res.render("client/auth/login.ejs")
+}
+
+export { getLoginPage, getRegisterPage }
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,5 +1,6 @@
 import { getAdminOrderPage, getAdminProductPage, getAdminUserPage, getDashboardPage } from 'controllers/admin/dashboard.controller'
 import { getAdminCreateProductPage, getViewProduct, postAdminCreateProduct, postDeleteProduct, postUpdateProduct } from 'controllers/admin/product.controller'
+import { getLoginPage, getRegisterPage } from 'controllers/client/auth.controller'
 import { getProductPage } from 'controllers/client/product.controller'
 import { getCreateUserPage, getHomePage, getViewUser, postCreateUserPage, postDeleteUser, postUpdateUser } from 'controllers/user.controller'
 import express, { Express } from 'express'
@@ -10,6 +11,8 @@ const router = express.Router()
 const webRoutes = (app: Express) => {
     router.get('/', getHomePage)
     router.get('/product/:id', getProductPage)
+    router.get('/login', getLoginPage)
+    router.get('/register', getRegisterPage)
 
     // Admin routes
     router.get('/admin', getDashboardPage)
